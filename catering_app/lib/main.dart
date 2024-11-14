35136f22cfdd4c04940918a304a626005e95219c
kawakey <karol.boreck@gmail.com>
2024-11-14 19:29:04 +0100

    removed debug banner

--- catering_app/lib/main.dart
+++ catering_app/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Catering app',
       theme: AppThemes.light,
       darkTheme: AppThemes.light,
